207df771059b6007c5627b9cc77eac52501ee29f
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-22 00:49:58 +0900

    Create RepeatBackGround.cs

---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private float _speed = 10.0f;
+    private Vector3 _startPos;
+
+    private float _repeatWidth;
+    void Start()
+    {
+        _startPos = transform.position;
+        _repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    void Update()
+    {
+        if(transform.position.x < _startPos.x - _repeatWidth)
+        {
+            ResetPostion();
+        }
+        if(!PlayerController.gameOver)
+        {
+            MoveLeft();
+        }
+    }
+
+    void MoveLeft()
+    {
+        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+    }
+
+    void ResetPostion()
+    {
+        transform.position = _startPos;
+    }
+}

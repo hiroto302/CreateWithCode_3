304a6527bc3d61848ba5c12a9be9dcf17c3dd315
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-21 20:32:48 +0900

    Create PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody _rb;
+    public float JumpForce = 15;
+    public float gravityModifilter = 2.5f;
+    public bool isOnGround = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifilter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    /*
+        collision : 衝突したオブジェクトの情報を扱う
+    */
+
+    // collider/rigidbody は他の collider/rigidbody に触れたときに OnCollisionEnter は呼び出される
+    // In contrast to OnTriggerEnter, OnCollisionEnter is passed the Collision class and not a Collider.
+    void OnCollisionEnter(Collision collision)
+    {
+        IsOnGround(true);
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            _rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround(false);
+        }
+    }
+
+    void IsOnGround(bool isOnGround)
+    {
+        this.isOnGround = isOnGround;
+    }
+}

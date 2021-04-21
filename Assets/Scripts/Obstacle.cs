7a275fa020a6b062d3af4023ef8e2bf210ff33c6
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-21 23:54:32 +0900

    Create Obstacle.cs

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float hidePosX = -5f;
+
+    void Update()
+    {
+        MoveLeft();
+        Hide();
+    }
+
+    void MoveLeft()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+
+    void Hide()
+    {
+        if(hidePosX > transform.position.x)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
